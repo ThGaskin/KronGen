f2cf15c2c2a0265d1dd2dfd03f22f88d385cbff3
Thomas Gaskin <tgaskin@live.co.uk>
2021-12-09 00:17:20 +0000

    Add graph types and string to graph type conversion function [skip ci]

--- src/models/KronGen/graph_types.hh
+++ src/models/KronGen/graph_types.hh
@@ -8,7 +8,10 @@ enum GraphType {
     Chain,
     Complete,
     ErdosRenyi,
-    Regular
+    KlemmEguiluz,
+    Regular,
+    ScaleFree,
+    SmallWorld
 };
 
 // Convenient printing function for graph types
@@ -16,8 +19,38 @@ std::string Graph_Type[] = {
     "Chain",
     "Complete",
     "Erdos-Renyi",
-    "Regular"
+    "Regular",
+    "Scale-free",
+    "Small-world"
 };
+
+// Convert string to GraphType
+GraphType to_graphtype(std::string s) {
+    if (s == "BarabasiAlbert" or s == "scale-free"){
+        return ScaleFree;
+    }
+    else if (s == "chain") {
+        return Chain;
+    }
+    else if (s == "complete") {
+        return Complete;
+    }
+    else if (s == "ErdosRenyi") {
+        return ErdosRenyi;
+    }
+    else if (s == "KlemmEguiluz") {
+        return KlemmEguiluz;
+    }
+    else if (s == "regular" or s == "Regular") {
+        return Regular;
+    }
+    else if (s == "WattsStrogatz"){
+        return SmallWorld;
+    }
+    else {
+        return ErdosRenyi;
+    }
+}
 
 } // namespace KronGen::GraphTypes
 
